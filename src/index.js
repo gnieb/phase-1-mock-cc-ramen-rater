2b7ffd42ce2b7dc7482cea4a54d674bfcd587ef0
Grace Nieboer <grace.nieboer@outlook.com>
2023-02-08 21:45:41 -0500

    practice before code challenge

--- src/index.js
+++ src/index.js
@@ -1,4 +1,61 @@
-const ramenMenuContainer = document.getElementById('ramen-menu')
+//when DOM loads,display ramen images using img tagin ramen-menu //fetch GET
+
+const ramenMenuContainer = document.getElementById('ramen-menu');
+const ramenNameH2 = document.querySelector('h2.name')
+const ramenRestH3 = document.querySelector('h3.restaurant')
+const ramenImageContainer = document.getElementById('display-one-image')
+const ramenRatingContainer = document.getElementById('rating-display')
+const ramenCommentContainer = document.getElementById('comment-display')
+const newRamenForm = document.getElementById('new-ramen')
+
+
+fetch("http://localhost:3000/ramens")
+.then(resp=> resp.json())
+.then((arrayOfObjects)=> {
+    arrayOfObjects.forEach(renderRamen)
+})
+
+
+function renderRamen (ramenObject) {
+    const ramenImage = document.createElement('img');
+    ramenImage.alt = ramenObject.name;
+    ramenImage.src = ramenObject.image
+    ramenMenuContainer.append(ramenImage)
+
+    ramenImage.addEventListener('click', () => {
+    ramenNameH2.innerText = ramenObject.name;
+    ramenRestH3.innerText = ramenObject.restaurant;
+    ramenImageContainer.src = ramenObject.image;
+    ramenRatingContainer.innerText = ramenObject.rating;
+    ramenCommentContainer.innerText = ramenObject.comment
+    })
+}
+
+newRamenForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    console.log(e)
+    const newRamen = {
+        name: e.target.name.value,
+        restaurant: e.target.restaurant.value,
+        image: e.target.image.value,
+        rating: e.target.rating.value,
+        comment: e.target['new-comment'].value,
+    }
+    renderRamen(newRamen)
+    newRamenForm.reset()
+})
+
+
+
+
+
+
+////////////////////////SOLUTION CODE /////////////////////
+////////////////////////SOLUTION CODE /////////////////////
+////////////////////////SOLUTION CODE /////////////////////
+
+
+/*const ramenMenuContainer = document.getElementById('ramen-menu')
 const commentContainer = document.getElementById('comment-display')
 const ratingContainer = document.getElementById('rating-display')
 const displayImage = document.getElementById('display-one-image')
@@ -44,4 +101,4 @@ newRamenForm.addEventListener('submit', (e) => {
     newRamenForm.reset() 
 }
 )
-
+*/
